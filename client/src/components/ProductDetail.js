43f8e678cc275de97852708237a533cb6394e748
Mortron <cmsl4y3r@gmail.com>
2021-12-12 20:52:01 -0700

    delete added to details and products 3 complete!

--- client/src/components/ProductDetail.js
+++ client/src/components/ProductDetail.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from 'axios';
+import { navigate } from "@reach/router";
 
 const ProductDetail = (props) => {
     const [product, setProduct ] = useState({});
@@ -9,12 +10,21 @@ const ProductDetail = (props) => {
             .then((res)=> setProduct({...res.data}))
     }, [])
 
+    const deleteProduct = (productId) => {
+        axios.delete(`http://localhost:8000/api/products/${productId}`)
+            .then((res) => {
+                console.log(res.data);
+                navigate('/products');
+            })
+            .catch((err) => console.log(err));
+    }
 
     return(
         <div>
             <p>Product Title: {product.title}</p>
             <p>Product Price: {product.price}</p>
             <p>Product Description: {product.description}</p>
+            <button onClick={() => {deleteProduct(product._id)}}>Delete</button>
         </div>
     )
 }
